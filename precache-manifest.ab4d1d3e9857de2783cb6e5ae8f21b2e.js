be99e31362bc5fc2042aa9f35656b75ef8ebe0ca
sdob <sdob@cs.ucc.ie>
2018-10-24 08:55:33 +0100

    Updates

---
+++ precache-manifest.ab4d1d3e9857de2783cb6e5ae8f21b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "348fc98eaa9c4acf8075",
+    "url": "/react-interactive-test/static/js/runtime~main.348fc98e.js"
+  },
+  {
+    "revision": "13417e97ea2f4cbc5150",
+    "url": "/react-interactive-test/static/js/main.13417e97.chunk.js"
+  },
+  {
+    "revision": "29b86adea67c6a88eb96",
+    "url": "/react-interactive-test/static/js/1.29b86ade.chunk.js"
+  },
+  {
+    "revision": "13417e97ea2f4cbc5150",
+    "url": "/react-interactive-test/static/css/main.3b721954.chunk.css"
+  },
+  {
+    "revision": "a0564fb3a70b4ff586fd6b841b04e822",
+    "url": "/react-interactive-test/index.html"
+  }
+];
